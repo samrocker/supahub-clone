6d2ba5ba7e555481bc396954826a6d382bf2f962
samrocker <samrockerislive04@gmail.com>
2024-09-05 23:07:19 +0530

    hero top-0 section complete with responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeaderSection from '@/components/common/HeaderSection';
+import HeroSection from '@/components/LandingSection/HeroSection';
 import react from 'react';
 
 export default function Home() {
   return (
     <>
     <HeaderSection />
+    <HeroSection />
     </>
   );
 }
--- src/components/LandingSection/HeroSection.tsx
+++ src/components/LandingSection/HeroSection.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../ui/button";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <section>
+      <main>
+        <div className="py-5 px-5 min-h-screen w-full flex-center flex-col">
+          <div className="flex-center flex-col gap-5">
+            <span className="text-[#862FE7] text-xs md:text-sm lg:text-lg font-bold uppercase">
+              Product & Customer Feedback Tool
+            </span>
+            <h1 className="text-primary text-2xl md:text-4xl lg:text-6xl font-semibold text-center max-w-[650px]">
+              Central hub to collect feedback & announce product updates
+            </h1>
+            <p className="text-[#3F4654] text-xs md:text-sm lg:text-lg font-semibold text-center max-w-[571px]">
+              With Supahub collect, organize and prioritize feature requests to
+              better understand customer feedback and use them to inform your
+              product roadmap.
+            </p>
+            <div className="flex-between flex-row gap-5">
+              <Button className="text-white text-xs md:text-sm lg:text-lg flex-between gap-1 md:gap-2 font-semibold px-3 md:px-5 py-6 lg:py-7 bg-primary rounded-sm">
+                <Image src='/icons/button-star.png' alt="" width={23} height={23} className="hidden lg:block" />
+                <Image src='/icons/button-star.png' alt="" width={15} height={15} className="block lg:hidden" />
+                Sign up for free
+                </Button>
+                <Button className="text-primary text-xs md:text-sm lg:text-lg flex-between gap-2 font-semibold px-3 md:px-5 py-6 lg:py-7 bg-transparent rounded-sm border-[1px] border-primary/20">
+                See Supahub in action
+                </Button>
+            </div>
+            <div className="flex-center gap-3">
+              <div className="flex-between gap-2">
+                <Image src='/icons/credit-card-icon.png' alt='' height={20} width={20} />
+                <h1 className="text-xs md:text-sm font-semibold text-[#495060]">No credit card required</h1>
+              </div>
+              <div className="flex-center relative">
+              <Image src='/icons/dot-icon.png' alt='' height={5} width={5} objectFit="absolute top-50 left-50 cover" />
+              </div>
+              <div className="flex-between gap-2">
+                <Image src='/icons/timer-icon.png' alt='' height={20} width={20} />
+                <h1 className="text-xs md:text-sm font-semibold text-[#495060]">14-day free trial</h1>
+              </div>
+            </div>
+          </div>
+          <div className="flex-center flex-col gap-5"></div>
+        </div>
+      </main>
+    </section>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
