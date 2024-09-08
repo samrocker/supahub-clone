1a233875a9b14d17051e77d132fddb93892ac8a6
samrocker <samrockerislive04@gmail.com>
2024-09-09 03:09:39 +0530

    incomplete footer

--- src/components/common/FooterSection.tsx
+++ src/components/common/FooterSection.tsx
@@ -1,10 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
 const FooterSection = () => {
   return (
     <section>
-        <main> 
-            <div></div>
+        <main className='max-w-[1200px] m-auto'> 
+            <div className='py-5 px-5 w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-content-between border-t-[1px] boder-[#0B3D79]/50'>
+              <div className='py-5 w-full flex-center flex-col gap-3'>
+                <Link href='/'></Link>
+                <Link href='/'></Link>
+                <Link href='/'></Link>
+                <Link href='/'></Link>
+              </div>
+              <div className='py-5 w-full flex-center flex-col gap-3'></div>
+              <div className='py-5 w-full flex-center flex-col gap-3'></div>
+              <div className='py-5 w-full flex-center flex-col gap-3'></div>
+            </div>
         </main>
     </section>
   )
